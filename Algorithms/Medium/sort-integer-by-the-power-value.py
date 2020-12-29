c2821d4e12d9c636b54eeb43d73dfbabf6160a39
heyitschun <cpoon19@gmail.com>
2020-12-29 19:15:16 +0100

    upd

---
+++ Algorithms/Medium/sort-integer-by-the-power-value.py
@@ -0,0 +1,29 @@
+"""
+The power of an integer `x` is defined as the number of steps needed to
+transform `x` into `1` using the following steps:
+
+- if x is even then x = x / 2
+- if x is odd then x = 3 * x + 1
+
+Given three integers, lo, hi and k, the task is to sort all integers in the 
+interval [lo, hi] by the power value in ascending order, if two or more
+intergers have the same power value sort them by ascending order.
+
+Return the kth integer in the range sorted by the power value.
+
+Example
+-------
+
+lo = 12
+hi = 15
+k = 2
+
+>>> 13
+"""
+
+def getKth(lo, hi, k):
+    return 
+
+lo = 12
+hi = 15
+k = 2
